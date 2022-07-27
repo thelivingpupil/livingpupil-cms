c4c21aa5e3e12d88ce2cddd902acc096dd5ad21c
Sylvie <sylvie@Sylvie.local>
2022-07-27 08:14:22 +0800

    Add applicable grade level to courses

--- schemas/documents/courses.js
+++ schemas/documents/courses.js
@@ -1,5 +1,24 @@
 import { FaCertificate } from 'react-icons/fa';
 
+const gradesList = [
+  'ALL',
+  'PRESCHOOL',
+  'K1',
+  'K2',
+  'GRADE_1',
+  'GRADE_2',
+  'GRADE_3',
+  'GRADE_4',
+  'GRADE_5',
+  'GRADE_6',
+  'GRADE_7',
+  'GRADE_8',
+  'GRADE_9',
+  'GRADE_10',
+  'GRADE_11',
+  'GRADE_12',
+];
+
 export default {
   name: 'courses',
   title: 'Courses',
@@ -10,6 +29,12 @@ export default {
       name: 'code',
       title: 'Course Code',
       type: 'string',
+    },
+    {
+      name: 'gradeLevel',
+      title: 'Applicable Grade Level',
+      type: 'string',
+      options: { list: gradesList },
     },
     {
       name: 'title',
@@ -31,6 +56,12 @@ export default {
       title: 'Lessons',
       type: 'array',
       of: [{ type: 'lesson' }],
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'string' }],
     },
   ],
 };
