ed2ef14b24ab4a2db49a1d960c2337f42fc66d39
Gideon Arces <gid.arces07@gmail.com>
2023-05-20 13:13:02 +0800

    Adding books for virtual library

---
+++ schemas/documents/books.js
@@ -0,0 +1,51 @@
+import { FaBookOpen } from 'react-icons/fa';
+
+const bookForTypes = [
+  { title: 'Parents', value: 'PARENTS' },
+  { title: 'Students', value: 'STUDENTS' },
+];
+
+export default {
+  name: 'books',
+  title: 'Books',
+  type: 'document',
+  icon: FaBookOpen,
+  fields: [
+    {
+      name: 'bookTitle',
+      title: 'Book Title',
+      type: 'string',
+    },
+    {
+      name: 'bookFor',
+      title: 'Book For',
+      type: 'string',
+      options: {
+        list: bookForTypes,
+      },
+    },
+    {
+      name: 'bookCover',
+      title: 'Book Cover',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'caption',
+          title: 'Caption',
+          type: 'string',
+          options: {
+            isHighlighted: true,
+          },
+        },
+      ],
+    },
+    {
+      name: 'bookFile',
+      title: 'Book File',
+      type: 'file',
+    },
+  ],
+};
