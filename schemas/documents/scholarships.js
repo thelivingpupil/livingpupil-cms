8f8ee80fc70ae1f790db2f68db37ba74222dbc37
julo07 <marjulmugot@gmail.com>
2024-05-03 03:26:13 +0800

    add scholarships

---
+++ schemas/documents/scholarships.js
@@ -0,0 +1,47 @@
+import { FaPercent } from 'react-icons/fa';
+
+const scholarshipTypes = [
+  { title: 'Percentage (%)', value: 'PERCENTAGE' },
+  { title: 'Value (Php)', value: 'VALUE' },
+];
+
+export default {
+  name: 'discounts',
+  title: 'Discounts',
+  type: 'document',
+  icon: FaPercent,
+  fields: [
+    {
+      name: 'code',
+      title: 'Scholarship Code',
+      type: 'string',
+    },
+    {
+      name: 'type',
+      title: 'Scholarship Type',
+      type: 'string',
+      initialValue: 'VALUE',
+      options: {
+        list: scholarshipTypes,
+      },
+    },
+    {
+      name: 'value',
+      title: 'Value',
+      type: 'number',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'count',
+      title: 'Inventory Count',
+      type: 'number',
+    },
+  ],
+  initialValue: {
+    count: 0,
+  },
+};
