15178ab482c97106d0d3a2fe39727403a90d0a0d
julo07 <114547558+julo07@users.noreply.github.com>
2024-07-04 23:12:00 +0800

    add grade level to blueprint

--- schemas/documents/blueprint.js
+++ schemas/documents/blueprint.js
@@ -7,6 +7,19 @@ const formLevelTypes = [
   { title: 'Form 3', value: 'FORM_3' },
 ];
 
+const gradeLevelTypes = [
+  { title: 'Grade 1', value: 'GRADE_1' },
+  { title: 'Grade 2', value: 'GRADE_2' },
+  { title: 'Grade 3', value: 'GRADE_3' },
+  { title: 'Grade 4', value: 'GRADE_4' },
+  { title: 'Grade 5', value: 'GRADE_5' },
+  { title: 'Grade 6', value: 'GRADE_6' },
+  { title: 'Grade 7', value: 'GRADE_7' },
+  { title: 'Grade 8', value: 'GRADE_8' },
+  { title: 'Grade 9', value: 'GRADE_9' },
+  { title: 'Grade 10', value: 'GRADE_10' },
+];
+
 const programTypes = [
   { title: 'Homeschool Program', value: 'HOMESCHOOL_PROGRAM' },
   { title: 'Homeschool Cottage', value: 'HOMESCHOOL_COTTAGE' },
@@ -18,12 +31,20 @@ export default {
   type: 'document',
   icon: FaClipboardList,
   fields: [
+    // {
+    //   name: 'formLevel',
+    //   title: 'Form Level',
+    //   type: 'string',
+    //   options: {
+    //     list: formLevelTypes,
+    //   },
+    // },
     {
-      name: 'formLevel',
-      title: 'Form Level',
+      name: 'gradeLevel',
+      title: 'Grade Level',
       type: 'string',
       options: {
-        list: formLevelTypes,
+        list: gradeLevelTypes,
       },
     },
     {
@@ -42,11 +63,11 @@ export default {
   ],
   preview: {
     select: {
-      form: 'formLevel',
+      grade: 'gradeLevel',
       program: 'programType',
     },
     prepare: (selection) => ({
-      title: `${selection.program || 'Both'} - ${selection.form}`,
+      title: `${selection.program || 'Both'} - ${selection.grade}`,
     }),
   },
 };
